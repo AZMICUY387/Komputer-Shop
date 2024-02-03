136c97097b8b6a8b776c5d017a052bfa9dc4dd73
PAYAD <55588164+AZMICUY387@users.noreply.github.com>
2024-02-03 16:06:02 +0700

    Add files via upload

---
+++ komputer_shop.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 03 Feb 2024 pada 10.05
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `komputer_shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `account`
+--
+
+CREATE TABLE `account` (
+  `Username` varchar(11) NOT NULL,
+  `Password` varchar(11) NOT NULL,
+  `Phone Number` int(15) NOT NULL,
+  `Email` varchar(32) NOT NULL,
+  `Age` int(4) NOT NULL,
+  `Address` varchar(128) NOT NULL,
+  `Photo` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `account`
+--
+
+INSERT INTO `account` (`Username`, `Password`, `Phone Number`, `Email`, `Age`, `Address`, `Photo`) VALUES
+('az', 'az', 0, '', 0, '', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
